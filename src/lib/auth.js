e72ab09ea9468f55b5a91178fae5b5a7cb70e17f
Rabiul-Sujon <alamrabiul889@gmail.com>
2026-05-02 21:53:08 +0600

    fix auth build error

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -27,14 +27,31 @@
 //   },
 // });
 
+// import { betterAuth } from "better-auth";
+// import { mongodbAdapter } from "better-auth/adapters/mongodb";
+// import clientPromise from "./mongodb";
+
+// const client = await clientPromise;
+
+// export const auth = betterAuth({
+//   database: mongodbAdapter(client.db("borrowbooks")),
+//   emailAndPassword: {
+//     enabled: true,
+//   },
+//   socialProviders: {
+//     google: {
+//       clientId: process.env.GOOGLE_CLIENT_ID,
+//       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+//     },
+//   },
+// });
+
 import { betterAuth } from "better-auth";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 import clientPromise from "./mongodb";
 
-const client = await clientPromise;
-
 export const auth = betterAuth({
-  database: mongodbAdapter(client.db("borrowbooks")),
+  database: mongodbAdapter(await clientPromise.then(c => c.db("borrowbooks"))),
   emailAndPassword: {
     enabled: true,
   },
